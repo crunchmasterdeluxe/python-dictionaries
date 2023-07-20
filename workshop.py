54dd372806c46b51838f8b38350df7ee94c82ec8
Andy Gannaway <83776204+crunchmasterdeluxe@users.noreply.github.com>
2023-07-20 15:43:51 -0600

    Create workshop.py

---
+++ workshop.py
@@ -0,0 +1,46 @@
+# A dictionary, like a json object, is an object of key-value pairs.
+# Ref https://www.w3schools.com/python/python_dictionaries.asp for dictionary basics
+def get_dictionary():
+  return {
+    "birth_year": "19 BBY",
+    "eye_color": "Blue",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+    ],
+    "gender": "Male",
+    "hair_color": "Blond",
+    "height": "172",
+    "homeworld": "https://swapi.dev/api/planets/1/",
+    "mass": "77",
+    "name": "Luke Skywalker",
+    "skin_color": "Fair",
+    "created": "2014-12-09T13:50:51.644000Z",
+    "edited": "2014-12-10T13:52:43.172000Z",
+    "url": "https://swapi.dev/api/people/1/"
+}
+
+# define a dictionary
+d = get_dictionary()
+
+# 1. Without looping, print the dictionary's items (key-value pairs)
+
+
+# 2. Without looping, print the dictionary's keys
+
+
+# 3. Without looping, print the dictionary's values
+
+
+# 4. Add a new item to the dictionary
+
+
+# 5. Loop through the dictionary and print out the keys
+
+
+# 6. As you loop through the dictionary, print the key and its value
+
+
+# 7. Print the length of the dictionary (how many items does it contain?)
+
